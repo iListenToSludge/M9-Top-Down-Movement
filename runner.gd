a0fcef62679ddc5dcb9fa70a364f4a636b3e2e99
ZACKBOY <sevang@s.sfusd.edu>
2025-12-18 12:42:03 -0800

    finished m9 l8

--- runner.gd
+++ runner.gd
@@ -1,4 +1,7 @@
+class_name Runner
 extends CharacterBody2D
+signal walked_to
+
 
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 @onready var dust: GPUParticles2D = %Dust
@@ -7,6 +10,21 @@ extends CharacterBody2D
 @export var acceleration := 1200.0
 @export var deceleration := 1080.0
 
+
+func walk_to(destination_global_position: Vector2) -> void:
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	dust.emitting = true
+	var distance := global_position.distance_to(destination_global_position)
+	var _duration :=  distance / (max_speed * 0.2)
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, _duration)
+	tween.finished.connect(func():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		dust.emitting = false
+		walked_to.emit()
+	)
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
